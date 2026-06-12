4ddde73c2486349ff51be267d97e27593c1eed4a
kemi-p <kpole@dvtsoftware.com>
2026-06-12 11:25:15 +0200

    feat: abstract class for the elevator types

--- ElevatorSim.Domain/Entities/ElevatorBase.cs
+++ ElevatorSim.Domain/Entities/ElevatorBase.cs
@@ -1,1 +1,42 @@
 namespace ElevatorSim.Domain.Entities;
+
+using ElevatorSim.Domain.Enums;
+using ElevatorSim.Domain.Exceptions;
+using ElevatorSim.Domain.Interfaces;
+
+public abstract class ElevatorBase : IElevator
+{
+    private int _passengerCount;
+    public int Id { get; }
+    public int CurrentFloor { get; set; }
+    public ElevatorDirection Direction { get; set; } = ElevatorDirection.Stationary;
+
+    public ElevatorState State { get; set; } = ElevatorState.Available;
+
+    public int PassengerCount => _passengerCount;
+
+    public abstract int MaxCapacity { get; }
+
+    public bool IsAvailable => State == ElevatorState.Available || State == ElevatorState.DoorsOpen;
+
+    public ElevatorBase(int id)
+    {
+        Id = id;
+        CurrentFloor = 1;
+    }
+
+    public abstract Task MoveToFloor(int floor);
+
+    public void BoardingPassengers(int count)
+    {
+        if (_passengerCount + count > MaxCapacity)
+            throw new CapacityExceededException(MaxCapacity);
+
+        _passengerCount += count;
+    }
+
+    public void ExitingPassengers(int count)
+    {
+        _passengerCount = Math.Max(0, _passengerCount - count);
+    }
+}
