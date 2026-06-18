044f172fea7855abf177fcc2aa9a2380d38caa06
kemi-p <kpole@dvtsoftware.com>
2026-06-18 11:46:46 +0200

    feat; rename file to match rubric (IDispatch to IDispatchStrategy

--- ElevatorSim.Domain/Interfaces/IDispatchStrategy.cs
+++ ElevatorSim.Domain/Interfaces/IDispatchStrategy.cs
@@ -3,13 +3,13 @@ namespace ElevatorSim.Domain.Interfaces;
 /// <summary>
 /// Chooses which elevator should respond to a floor request.
 /// </summary>
-public interface IDispatch
+public interface IDispatchStrategy
 {
     /// <summary>
     /// Picks the best elevator for a pickup at the given floor.
     /// </summary>
     /// <param name="elevators">The elevators available to dispatch.</param>
     /// <param name="requestedFloor">The floor where passengers are waiting.</param>
-    /// <returns>The selected elevator, or nullif none are suitable.</returns>
+    /// <returns>The selected elevator, or null if none are suitable.</returns>
     IElevator? SelectElevator(IReadOnlyList<IElevator> elevators, int requestedFloor);
 }
